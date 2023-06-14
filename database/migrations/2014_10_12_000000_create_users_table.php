ba616d0023a6cc4a29dd0d110d9353f12b00e4bb
tsunodajapa <gabrieltsunoda@gmail.com>
2023-06-14 13:59:43 -0300

    feat: migrations

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -6,25 +6,20 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('cpf')->unique();
             $table->string('password');
-            $table->rememberToken();
+            $table->string('type', 6);
+            $table->decimal('balance')->default(0);
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('users');
---
+++ database/migrations/2023_06_13_195603_create_transactions_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->decimal('value');
+            $table->timestamps();
+            $table->unsignedBigInteger('payer_id');
+            $table->unsignedBigInteger('payee_id');
+
+            $table->foreign('payer_id')->references('id')->on('users');
+            $table->foreign('payee_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transactions');
+    }
+};
